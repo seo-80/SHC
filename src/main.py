166873862c23b4d74d0732b3999bdf10479876c7
seo-80 <80seo717123@gmail.com>
2023-05-07 18:19:55 +0900

    speaker class

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 class shc():
     def __init__(self,kappa,rambda,rho=False,x0=[0,0,0]) -> None:
@@ -40,30 +41,46 @@ class rho:
 def S(x,G0=50,beta=0.5):
     return G0/(1+np.exp(-1*beta*x))
 
+class recognizer(shc):
+    def __init__(self, kappa, rambda, rho=False, x0=[0, 0, 0]) -> None:
+        super().__init__(kappa, rambda, rho, x0)
+        self.sigma=0.0
+    def update_sigma(self):
+        self.sigma
+class speaker():
+    def __init__(self,x0=False) -> None:
+        if  x0:
+            self.x2=(np.array(x0[0]))
+            self.x1=(np.array(x0[1]))           
+        else:
+            self.x2=(np.array([[random.randint(-30,0)],[random.randint(-30,0)],[random.randint(-30,0)]]))
+            self.x1=(np.array([[random.randint(-30,0)],[random.randint(-30,0)],[random.randint(-30,0)],[random.randint(-30,0)]]))
+        self.v2=S(self.x2)
+        self.v1=S(self.x1)
+        self.rho2=rho([[0,1,2],[2,1,0],[1,2,0]])
+        self.rho1=rho([[0,1,2,3],[3,2,1,0],[1,2,3,0]])
+        self.shc2=shc(1/8,0.3,self.rho2,self.x2)
+        self.shc1=shc(1/2,0.3,self.rho1,self.x1)
+    def update(self):
+        self.shc2.update([1])
+        self.shc1.update(self.shc2.get_v())
+    def simulate(self,n):
+        for i in range(n-1):
+            self.update()
+            self.x2=np.append(self.x2,self.shc2.get_x().reshape(3,1),axis=1)
+            self.x1=np.append(self.x1,self.shc1.get_x().reshape(4,1),axis=1)
+            self.v2=np.append(self.v2,self.shc2.get_x().reshape(3,1),axis=1)
+            self.v1=np.append(self.v1,self.shc1.get_x().reshape(4,1),axis=1)
+        
 
-
-
-rho2=rho([[0,1,2],[2,1,0],[1,2,0]])
-rho1=rho([[0,1,2,3],[3,2,1,0],[1,2,3,0]])
-
-
-
-n=5000
-
-x2=S(np.array([[-25],[-20],[-10]]))
-x1=S(np.array([[-25],[-20],[-10],[-20]]))
-shc2=shc(1/8,0.3,rho2,x2)
-shc1=shc(1/2,0.3,rho1,x1)
-for i in range(n-1):
-    shc2.update([1])
-    shc1.update(shc2.get_v())
-    x2=np.append(x2,shc2.get_v().reshape(3,1),axis=1)
-    x1=np.append(x1,shc1.get_v().reshape(4,1),axis=1)
+n=1000
+speaker=speaker()
+speaker.simulate(n)
 plt.subplot(211)
 for i in range(3):
-    plt.plot(range(n),x2[i])
+    plt.plot(range(n),speaker.v2[i])
 plt.subplot(212)
 for i in range(4):
-    plt.plot(range(n),x1[i])
+    plt.plot(range(n),speaker.v1[i])
 
 plt.show()
