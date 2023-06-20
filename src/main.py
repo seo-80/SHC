7f050d07c0ff44a7450f407fbf79b6abf08ba27a
seo-80 <80seo717123@gmail.com>
2023-06-20 10:29:00 +0900

    刻みはばを追加した

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import time
+from scipy.integrate import odeint
 randam = np.random.default_rng()
 
 class shc():
@@ -77,7 +78,7 @@ class speaker():
             self.v1=np.append(self.v1,self.shc1.get_x().reshape(4,1),axis=1)
 
 class recognizer():
-    def __init__(self, x0=False,var=[[0.1,0.1],[0.1,0.1]]) -> None:
+    def __init__(self, x0=False,var=[[0.1,0.1],[0.1,0.1]],use_scipy=True) -> None:
         self.var=var
         self.x2=np.array([random.randint(-30,0),random.randint(-30,0),random.randint(-30,0)])
         self.x1=np.array([random.randint(-30,0),random.randint(-30,0),random.randint(-30,0),random.randint(-30,0)])
@@ -91,10 +92,11 @@ class recognizer():
         self.rho1=rho([[0,1,2,3],[3,2,1,0],[1,2,3,0]])
         self.shc2=shc(1/32,0.3,[0,0],self.rho2,self.x2)
         self.shc1=shc(1/8,0.3,[0,0],self.rho1,self.x1)
-        self.var_scaler=2#2にする。計算速度見たくてオーバフローしちゃう時に0とかにする
+        self.var_scaler=1#2にする。計算速度見たくてオーバフローしちゃう時に0とかにする
         self.x2_old=self.x2
         self.v2_old=self.v2
         self.x1_old=self.x1
+        self.use_scipy=use_scipy
 
 
     def update(self,v1,h):
@@ -116,12 +118,29 @@ class recognizer():
         self.x2_old=self.x2
         self.v2_old=self.v2
         self.x1_old=self.x1
+    def update_by_scipy(self,v1,Division_number=1):
+
+        self.v1=v1
+        f1_diff=np.array([self.rho1([1,0,0])@S(self.x1)/8])
+        f1_diff=np.append(f1_diff,[self.rho1([0,1,0])@S(self.x1)/8],axis=0)
+        f1_diff=np.append(f1_diff,[self.rho1([0,0,1])@S(self.x1)/8],axis=0)
+        t=np.linspace(0, 1, Division_number)
+        self.x1=odeint(lambda x,t:(self.shc1.differentiate(self.v2)+(self.v1-S(x))*S_diff(x)/(self.var[1][1]**self.var_scaler)-(x-(self.x1_old+self.shc1.differentiate(self.v2)))/(self.var[1][0]**self.var_scaler)),self.x1,t)[-1]
+        self.x2=odeint(lambda x,t:(S(x-self.x2_old)    +f1_diff@(self.x1-self.x1_old-self.shc1.differentiate(self.v2))/(self.var[1][0]**self.var_scaler)-(self.v2-S(x))/(var[0][1]**self.var_scaler)),self.x2   ,t)[-1]
+        self.v2=odeint(lambda x,t:(S(self.x2-self.x2_old)    +f1_diff@(self.x1-self.x1_old-self.shc1.differentiate(x))/(self.var[1][0]**self.var_scaler)-(x-S(self.x2))/(var[0][1]**self.var_scaler)),self.v2,t)[-1]
+        self.x2_old=self.x2
+        self.v2_old=self.v2
+        self.x1_old=self.x1
     def simulate(self,v1,n,Division_number=1):
         for i in range(n-1):
             if i%10==0:
                 print(i)
-            for j in range(Division_number):
-                self.update(v1[:,i],1/Division_number)
+                print(self.x1)
+            if self.use_scipy:
+                self.update_by_scipy(v1[:,i],Division_number=Division_number)
+            else:
+                for j in range(Division_number):
+                    self.update(v1[:,i],1/Division_number)
             self.x2_record=np.append(self.x2_record,self.x2.reshape(3,1),axis=1)
             self.x1_record=np.append(self.x1_record,self.x1.reshape(4,1),axis=1)
             self.v2_record=np.append(self.v2_record,self.v2.reshape(3,1),axis=1)
@@ -134,7 +153,7 @@ speaker=speaker(var=var)
 recognizer=recognizer(var=var)
 speaker.simulate(n)
 start=time.time()
-recognizer.simulate(speaker.v1,n,Division_number=10000)#分割数は10000は必要
+recognizer.simulate(speaker.v1,n,Division_number=100000)#分割数は10000は必要
 print(time.time()-start)
 
 
